424405c71029ac3bc53a15a79179b6f157bad165
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-08-19 11:49:57 +0200

    dev tools

--- src/store.js
+++ src/store.js
@@ -13,6 +13,9 @@ const rootReducer = combineReducers({
   // comments
 })
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store;
