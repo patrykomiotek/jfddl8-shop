6bd90ad8f429aedacc92e61fcb306828745186d5
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-08-19 09:48:42 +0200

    Updates

---
+++ precache-manifest.699a4a89dded9544f5d926d2b20284e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89c435fd6432b81f28c8f40b7b28451",
+    "url": "/jfddl8-shop/index.html"
+  },
+  {
+    "revision": "dbd4d8384ad9c615796f",
+    "url": "/jfddl8-shop/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "358a95d56124332dd9c0",
+    "url": "/jfddl8-shop/static/js/2.6f8b7be4.chunk.js"
+  },
+  {
+    "revision": "dbd4d8384ad9c615796f",
+    "url": "/jfddl8-shop/static/js/main.5967086f.chunk.js"
+  },
+  {
+    "revision": "d3d001e77de5ec199aa4",
+    "url": "/jfddl8-shop/static/js/runtime~main.7eeb3b39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jfddl8-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
