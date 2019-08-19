d887d3be24eba4c2f3599236b01a78c6b9652386
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-08-19 13:34:56 +0200

    basket bugfix

--- src/basket/reducer.js
+++ src/basket/reducer.js
@@ -7,10 +7,29 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case BASKET_ADD_PRODUCT:
-      return {
-        ...state,
-        products: state.products.concat(action.payload)
+      const product = {
+        id: action.payload.id,
+        name: action.payload.name,
+        quantity: 1,
+        price: action.payload.price
       }
+      const newProducts = [...state.products];
+      const productIndex = newProducts.findIndex((element) => element.id === product.id)
+
+      if (productIndex === -1) {
+        return {
+          ...state,
+          products: state.products.concat(product)
+        }
+      } else {
+        const modifiedProduct = newProducts[productIndex]
+        newProducts[productIndex] = {...modifiedProduct, quantity: modifiedProduct.quantity + 1}
+        return {
+          ...state,
+          products: newProducts
+        }
+      }
+
     default:
       return state
   }
--- src/products/reducers.js
+++ src/products/reducers.js
@@ -7,9 +7,9 @@ const INITIAL_STATE = {
 }
 
 const products = [
-  { name: "One", description: 'Super produkt', price: 123 },
-  { name: "Two", description: 'Super produkt2', price: 332 },
-  { name: "Three", description: 'Super produkt3', price: 321 }
+  { id: 1, name: "One", description: 'Super produkt', price: 123 },
+  { id: 2, name: "Two", description: 'Super produkt2', price: 332 },
+  { id: 3, name: "Three", description: 'Super produkt3', price: 321 }
 ]
 
 export default (state = INITIAL_STATE, action) => {
